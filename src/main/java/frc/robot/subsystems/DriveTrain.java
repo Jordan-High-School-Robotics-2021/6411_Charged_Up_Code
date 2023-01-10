9692ef3350dd498e95ce7729ef4d0eb52552dc5c
pyriteC <89157444+pyriteC@users.noreply.github.com>
2023-01-09 16:23:51 -0800

    initialized drive motors and created simple functions

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,14 +4,43 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
+
+private VictorSP leftFrontMotor;
+private VictorSP leftBackMotor;
+private VictorSP rightFrontMotor;
+private VictorSP rightBackMotor; 
+private MotorControllerGroup leftMotors;
+private MotorControllerGroup rightMotors;
+private DifferentialDrive drive;
   /** Creates a new DriveTrain. */
-  public DriveTrain() {}
+  public DriveTrain() {
+    leftFrontMotor = new VictorSP(Constants.LEFT_FRONT_MOTOR);
+    leftBackMotor = new VictorSP(Constants.LEFT_BACK_MOTOR);
+    rightFrontMotor = new VictorSP(Constants.RIGHT_FRONT_MOTOR);
+    rightBackMotor = new VictorSP(Constants.RIGHT_BACK_MOTOR);
+    leftMotors = new MotorControllerGroup(leftFrontMotor, leftBackMotor);
+    rightMotors = new MotorControllerGroup(rightFrontMotor, rightBackMotor);
+    drive = new DifferentialDrive(leftMotors, rightMotors);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void setRightMotors(double speed)
+  {
+    rightMotors.set(speed*Constants.DRIVE_TRAIN_SPEED);
+  }
+  public void setLeftMotors(double speed)
+  {
+    leftMotors.set(speed*Constants.DRIVE_TRAIN_SPEED);
   }
 }
